fbc64701591901ab0e145ddb660f6155ea390b20
Jordyn Marcellus <jordynmarcellus@gmail.com>
2019-08-14 13:49:54 -0400

    Adds signUpUser to mutation resolver; file re-formatting incoming

--- index.js
+++ index.js
@@ -1,27 +1,59 @@
-import {GraphQLServer} from 'graphql-yoga'
-import {prisma} from './generated/prisma-client'
+import { GraphQLServer } from "graphql-yoga";
+import { prisma } from "./generated/prisma-client";
+import cookieParser from "cookie-parser";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+// productionize this by pulling directly from env; temp stop-gap
+dotenv.config();
 
 const resolvers = {
-    Query: {
-        users(root, {email}, context) {
-            return context.prisma.users()
-        }
+  Query: {
+    users(root, { email }, context) {
+      return context.prisma.users();
     },
-    Mutation: {
-        createUser(root, {email, name}, context) {
-            return context.prisma.createUser({
-                name,
-                email
-            })
-        }
+  },
+  Mutation: {
+    async signUpUser(root, { email, password, name }, context) {
+      const massagedEmail = email.toLowerCase();
+      const hashedPass = await bcrypt.hash(password, 10);
+
+      const createdUser = await context.prisma.createUser({
+        name,
+        email: massagedEmail,
+        password: hashedPass,
+        permissions: { set: ["USER"] },
+      });
+      const token = jwt.sign(
+        { userId: createdUser.id },
+        process.env.JWT_SECRET
+      );
+      console.log(token);
+
+      // hey if soemthing fails in here we need to delete the user :D !
+
+      // this isn't working but that's ok...
+      context.response.cookie("token", token, {
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60 * 24 * 30,
+      });
+      return createdUser;
     },
-}
+  },
+};
 
 const server = new GraphQLServer({
-    typeDefs: './schema.graphql',
-    resolvers,
-    context: {
-        prisma
-    },
-})
-server.start(() => console.log("running on port 4000?")).catch((e) => console.error(e))
+  typeDefs: "./schema.graphql",
+  resolvers,
+  context: req => ({
+    ...req,
+    prisma,
+  }),
+});
+
+server.express.use(cookieParser());
+
+server
+  .start(details => console.log(`running on ${details.port}`))
+  .catch(e => console.error(e));
